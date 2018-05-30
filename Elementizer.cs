46da222342ef550e0b0feebeb576b4bda7c60c24
BC_Programming <bc_programming@hotmail.com>
2018-05-30 06:51:02 -0700

    add new file to have centralized Elementizer functions, including one to retrieve the Git hash value

---
+++ Elementizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BASeCamp.Elementizer
+{
+    public class Elementizer
+    {
+        /// <summary> Return the git hash value for this build.</summary>
+        public static string GetGitHash()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var attrs = asm.GetCustomAttributes<AssemblyMetadataAttribute>();
+            return attrs.FirstOrDefault(a => a.Key == "GitHash")?.Value;
+        }
+    }
+}
